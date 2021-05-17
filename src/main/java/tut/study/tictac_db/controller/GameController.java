c90ad43c37b4829e7714d023cc7a32d682c776bc
Andrii <andreww@ukr.net>
2021-05-18 01:54:21 +0300

    controllers break recursion

--- src/main/java/tut/study/tictac_db/controller/GameController.java
+++ src/main/java/tut/study/tictac_db/controller/GameController.java
@@ -3,6 +3,8 @@ package tut.study.tictac_db.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import tut.study.tictac_db.model.Game;
+import tut.study.tictac_db.model.GameSession;
+import tut.study.tictac_db.model.Player;
 import tut.study.tictac_db.service.GameService;
 
 import java.sql.Timestamp;
@@ -23,9 +25,37 @@ public class GameController {
         return gameService.getAll();
     }
 
+    @GetMapping("/{id}")
+    public Game get(@PathVariable int id) {
+        Game game = gameService.get(id);
+        for (GameSession session: game.getSessions()) {
+            session.setGame(makeNonRecursiveGame(game));
+            session.setPlayer(makeNonRecursivePlayer(session.getPlayer()));
+        }
+        return game;
+    }
+
     @PostMapping
     public void create(@RequestBody Game game) {
         game.setTimestamp(new Timestamp(System.currentTimeMillis()));
         gameService.save(game);
+    }
+
+    private Player makeNonRecursivePlayer(Player player) {
+        return Player.builder()
+                .email(player.getEmail())
+                .name(player.getName())
+                .id(player.getId())
+                .build();
+    }
+
+    private Game makeNonRecursiveGame(Game game) {
+        return Game.builder()
+                .timestamp(game.getTimestamp())
+                .steps(game.getSteps())
+                .fieldWidth(game.getFieldWidth())
+                .fieldHeight(game.getFieldHeight())
+                .id(game.getId())
+                .build();
     }
 }
--- src/main/java/tut/study/tictac_db/controller/PlayerController.java
+++ src/main/java/tut/study/tictac_db/controller/PlayerController.java
@@ -1,11 +1,15 @@
 package tut.study.tictac_db.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
+import tut.study.tictac_db.model.Game;
+import tut.study.tictac_db.model.GameSession;
 import tut.study.tictac_db.model.Player;
 import tut.study.tictac_db.service.PlayerService;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping("player")
 public class PlayerController {
@@ -16,14 +20,14 @@ public class PlayerController {
         this.playerService = playerService;
     }
 
-
-    @GetMapping
-    public List<Player> getAll() {
-        List<Player> players = playerService.getAll();
-//        players.stream()
-//                .map(Player::getSessions)
-//                .
-        return players;
+    @GetMapping("/{id}")
+    public Player get(@PathVariable int id) {
+        Player player = playerService.get(id);
+        for (GameSession session : player.getSessions()) {
+            session.setPlayer(makeNonRecursivePlayer(player));
+            session.setGame(makeNonRecursiveGame(session.getGame()));
+        }
+        return player;
     }
 
     @PostMapping
@@ -34,5 +38,23 @@ public class PlayerController {
     @PutMapping
     public void update(@RequestBody Player player) {
         playerService.save(player);
+    }
+
+    private Player makeNonRecursivePlayer(Player player) {
+        return Player.builder()
+                .email(player.getEmail())
+                .name(player.getName())
+                .id(player.getId())
+                .build();
+    }
+
+    private Game makeNonRecursiveGame(Game game) {
+        return Game.builder()
+                .timestamp(game.getTimestamp())
+                .steps(game.getSteps())
+                .fieldWidth(game.getFieldWidth())
+                .fieldHeight(game.getFieldHeight())
+                .id(game.getId())
+                .build();
     }
 }
